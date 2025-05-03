0ce07198bd1dca2a7eea9011b82e62d167d7bf4f
Max-im <lytsenko.maksim@gmail.com>
2025-05-03 20:43:37 +0500

    initial commit

---
+++ src/test/java/projects/java/onlinewallet/OnlineWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package projects.java.onlinewallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineWalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
